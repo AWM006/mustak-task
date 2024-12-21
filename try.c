54434a1efb8a1570fdd384391b9a0da988c7851e
AWM006 <sunitamondal7924@gmail.com>
2024-12-21 21:09:09 +0530

    made by mustak

--- try.c
+++ try.c
@@ -2,26 +2,21 @@
 #include <stdio.h>
 
 int main(){
-    int a , b, c, d ,e , f , g;
-    printf("enter yout salary \n");
-    scanf("%d",&a);
-
-    c = a/100*15;
-    d = a/100*10;
-    e = a/100*8;
-    f = a/100*5;
-
-    if(a>10000){
-    b = a/100*38;
-    g = a+b;
-    printf("your tax is    %d \n",f);
+    float b_s , b_a ,h_r_a , t_a , t_s  , a_s ;
+    float tax;
+    printf("enter the salary");
+    scanf("%f",&b_s);
+    b_a=b_s*0.15;
+    h_r_a=b_s*0.10;
+    t_a=b_s*0.08;
+    t_s=b_s + b_a + h_r_a + t_a;
+    if (t_s>=10000)
+    {
+        tax = b_s*0.05;
+        a_s = t_s - tax;
     }
     else{
-        b = a/100*33;
-        g = a +b;
+        a_s =t_s;
     }
-    printf("your DA is     %d \n",c);
-    printf("your HRA is    %d \n",d);
-    printf("your TA is     %d \n",e);
-    printf("your total salary is \n %d",g);
+    printf("%f is actual salary",a_s);
 }
